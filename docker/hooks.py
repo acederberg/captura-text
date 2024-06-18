085f103acbbd848863420dabdf42ed23bac9b575
Adrian Cederberg <adrn.cederberg123@gmail.com>
2024-06-18 15:09:38 -0600

    fix(router): Added router prefix.

--- docker/hooks.py
+++ docker/hooks.py
@@ -1,19 +1,20 @@
 # =========================================================================== #
 from typing import Type
 
-# --------------------------------------------------------------------------- #
 from app.views import AppView
 
 
 def captura_plugins_app(app_view: Type[AppView]):
 
+    # --------------------------------------------------------------------------- #
     from text_app import TextView
 
-    app_view.view_router.include_router(TextView.view_router)
+    app_view.view_router.include_router(TextView.view_router, prefix="/text")
 
 
 def captura_plugins_client(requests):
 
+    # --------------------------------------------------------------------------- #
     from text_client import TextCommands
 
     requests.typer_children.update(text=TextCommands)
